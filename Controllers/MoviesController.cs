4dbe519bed92cb2423c568c0967a8a953ce1d832
jandreshj <jandreshdzj@gmail.com>
2019-12-06 16:13:52 -0600

    Displays a list of customers and movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,8 +1,6 @@
 ﻿using MovieRent.Models;
-using System;
+using MovieRent.ViewModels;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MovieRent.Controllers
@@ -12,8 +10,34 @@ namespace MovieRent.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Sherk!" };
-            return View(movie);
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
         }
     }
 }
---
+++ ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MovieRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRent.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
